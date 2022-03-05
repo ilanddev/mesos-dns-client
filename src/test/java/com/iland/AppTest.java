b598f010d52179b8d9aafb1dcb858b77ffeb395f
Tag Spilman <tspilman@iland.com>
2022-03-04 17:58:20 -0700

    mvn archetype:generate

---
+++ src/test/java/com/iland/AppTest.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2021, iland Internet Solutions, Corp
+ *
+ * This software is licensed under the Terms and Conditions contained within the
+ * "LICENSE.txt" file that accompanied this software. Any inquiries concerning
+ * the scope or enforceability of the license should be addressed to:
+ *
+ * iland Internet Solutions, Corp
+ * 1235 North Loop West, Suite 800
+ * Houston, TX 77008
+ * USA
+ *
+ * http://www.iland.com
+ */
+
+package com.iland;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+  @Test
+  void forValueIsCaseInsensitive() {
+    assert (true);
+  }
+
+}
