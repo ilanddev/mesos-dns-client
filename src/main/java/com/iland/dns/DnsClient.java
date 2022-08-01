5f94429744a5b575705a5dc7148450ece2485fd5
Tag Spilman <tspilman@iland.com>
2022-08-01 15:21:03 -0600

    Refactor DnsClient#lookupServiceRecords

--- src/main/java/com/iland/dns/DnsClient.java
+++ src/main/java/com/iland/dns/DnsClient.java
@@ -1,6 +1,6 @@
 package com.iland.dns;
 
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,8 +18,8 @@ public interface DnsClient {
 	 * @return A list of {@link DnsRecord DNS records}
 	 * @throws MesosDnsException
 	 */
-	List<? extends DnsRecord> lookup(final String name, RecordType... recordTypes)
-			throws NamingException;
+	List<? extends DnsRecord> lookup(final String name,
+		RecordType... recordTypes) throws NamingException;
 
 	/**
 	 * Lookup service records (SRV records).
@@ -29,21 +29,20 @@ public interface DnsClient {
 	 * @throws MesosDnsException
 	 */
 	default List<SrvDnsRecord> lookupServiceRecords(final String name)
-			throws NamingException {
-		final List<SrvDnsRecord> records = lookup(name, RecordType.SRV).stream()
-				.filter(r -> r instanceof SrvDnsRecord).map(r -> (SrvDnsRecord) r)
-				.collect(Collectors.toList());
-
-		Collections.sort(records, (r1, r2) -> {
-			// the priority of the target host, lower value means more preferred.
-			final int priority = Integer.compare(r1.getPriority(), r2.getPriority());
-			// a relative weight for records with the same priority, higher value means higher chance of getting picked.
+		throws NamingException {
+		final Comparator<SrvDnsRecord> byPriorityAndWeight = (r1, r2) -> {
+			// the priority of the target host, lower value means more preferred
+			final int priority =
+				Integer.compare(r1.getPriority(), r2.getPriority());
+			// a relative weight for records with the same priority, higher value means higher chance of getting picked
 			return priority == 0 ?
-					Integer.compare(r2.getWeight(), r1.getWeight()) :
-					priority;
-		});
+				Integer.compare(r2.getWeight(), r1.getWeight()) :
+				priority;
+		};
 
-		return records;
+		return lookup(name, RecordType.SRV).stream()
+			.filter(r -> r instanceof SrvDnsRecord).map(r -> (SrvDnsRecord) r)
+			.sorted(byPriorityAndWeight).collect(Collectors.toList());
 	}
 
 }
